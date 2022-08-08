abe688425b354234100312cc1a4ebe8e4cecca5f
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-08-07 20:04:24 -0500

    Se agregaron imports y se esta creando una diccionario para hacer: sitioWeb=ip

---
+++ Ejercicio1/Ejercicio1.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Ejercicio1{
+    public static void main(String arga[]){
+        
+        HashMap<String, ArrayList<String>> tabla = new HashMap<String, ArrayList<String>>();
+    }
+}
