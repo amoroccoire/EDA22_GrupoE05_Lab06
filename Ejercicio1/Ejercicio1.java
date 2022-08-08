c993c6fcdc8397a95d9f7963388c06e6e07a23b5
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-08-07 20:08:18 -0500

    agregando flujo de entrada para recibir sitios web e ip's

--- Ejercicio1/Ejercicio1.java
+++ Ejercicio1/Ejercicio1.java
@@ -1,9 +1,20 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Scanner;
 
-public class Ejercicio1{
+public class Ejercicio1 {
     public static void main(String arga[]){
         
+        Scanner sc = new Scanner(System.in);
         HashMap<String, ArrayList<String>> tabla = new HashMap<String, ArrayList<String>>();
+        System.out.println("Para salir escriba: 0");
+        While(true) {
+            System.out.print("Sitio Web");
+            String direccion = sc.next();
+            if (direccion.equals("0"))
+                break;
+            System.out.print("IP: ");
+            String ip = sc.next();
+        }
     }
 }
